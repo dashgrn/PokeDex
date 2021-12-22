26e181af84d14e92664a260c4b6e7301c9f8ac24
Jose Fernandez <josesk20@gmail.com>
2021-12-22 18:35:50 -0500

    initial status

---
+++ src/firebase/firebaseCgf.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBe45xQg49vZtYqHSx-PlDUO4KXT3qVO8U",
+  authDomain: "pokedex-5e5e5.firebaseapp.com",
+  projectId: "pokedex-5e5e5",
+  storageBucket: "pokedex-5e5e5.appspot.com",
+  messagingSenderId: "715870636403",
+  appId: "1:715870636403:web:34337000db36f128ef5d06"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
