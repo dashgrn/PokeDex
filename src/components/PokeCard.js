0aa0fa72403845f398d4071d56d70a42aea056c6
Jose Fernandez <josesk20@gmail.com>
2021-12-23 11:55:14 -0500

    get pkm

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,13 +1,59 @@
+import axios from 'axios'
 import { useFormik } from 'formik'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Card, Form } from 'react-bootstrap'
 import { useDispatch } from 'react-redux'
 import { searchAsync } from '../actions/pokeActions'
 
+const getAllPkmURL = 'https://pokeapi.co/api/v2/pokemon/?limit=25'
 
 export const PokeCard = () => {
 
     const dispatch = useDispatch()
+
+    const [getNextPg, setGetNextPg] = useState('')
+
+    const [pkmData, setPkmData] = []
+
+    //get all pkm (individual url) HELPER FUNCTION
+    const getAllPkm = async (url) => {
+        await axios.get(url)
+            .then( res => {
+                console.log('all data res', res.data)
+                return res.data
+            })
+            .catch( err => {console.log('error',err)})
+    }
+
+    //get individual pkm from url  HELPER FUNCTION
+    const pkmDataFetcher = async (url) => {
+        await axios.get(url)
+            .then(res => {
+
+                return res.data
+            })
+            .catch(err => { console.log('error', err) })
+    }
+    
+    const loadPkm = async (data) => {
+        console.log('data as params', data)
+        let pokemonArr = data.map( async pkm => {
+            let pokeKeeper = pkmDataFetcher(pkm.url)
+            return pokeKeeper
+        })
+
+        setPkmData(pokemonArr)
+    }
+
+
+    useEffect(() => {
+        const fetchPkm = async (url) => {
+            let res = await getAllPkm(url)
+            console.log('res useEffect', res)
+        }
+        fetchPkm(getAllPkmURL)
+    }, [])
+
 
     const formik = useFormik({
         initialValues: {
@@ -20,10 +66,9 @@ export const PokeCard = () => {
         }
     })
 
-
-
     return (
         <>
+
             <form onSubmit={formik.handleSubmit}>
                 <Form.Control
                     value={formik.values.query}
@@ -34,7 +79,7 @@ export const PokeCard = () => {
                 />
             </form>
 
-            <Card className="m-6" style={{ width: '18rem' }}>
+            <Card className="mx-6" style={{ width: '18rem' }}>
                 <Card.Img variant="top" src="holder.js/100px180" />
                 <Card.Body>
                     <Card.Title>Pokemon Name</Card.Title>
@@ -44,9 +89,13 @@ export const PokeCard = () => {
                     <Card.Text>
                         Type: Fire
                     </Card.Text>
-                    <Button variant="primary">Details</Button>
+                    <Button variant="info">Details</Button>
                 </Card.Body>
             </Card>
+
+            <Button className="btn btn-warning m-2">
+                Cargar Más
+            </Button>
         </>
     )
 }
